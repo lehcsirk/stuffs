ea6fddf24032d4a05d2e7dc6c071a6a0eabda1bd
Cameron Krischel <ckrischel@he3labs.com>
2020-02-29 11:49:18 -0800

    Initial Commit

---
+++ Stuffs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stuffs
+//
+//  Created by Cameron Krischel on 2/29/20.
+//  Copyright © 2020 Cameron Krischel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
